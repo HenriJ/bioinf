73e23097394df493991ad4f76cd88287efb26da1
Riton <riton@melix.net>
2011-02-01 14:08:32 +0100

    Ignoring IDE's project files
    Signed-off-by: Riton <riton@melix.net>

---
+++ src/BioInf.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : BioInf.cpp
+// Author      : Introduction au C++
+// Version     :
+// Copyright   :
+// Description :
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello BioInf!!!" << endl;
+	return 0;
+}
