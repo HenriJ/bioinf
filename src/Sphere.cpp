3e0335d0e519d8ef75df1edc97ecbe9e7c90499d
samuel <samuel.rosat@gmail.com>
2011-02-15 16:42:53 +0100

    ajout de "Sphere"

---
+++ src/Sphere.cpp
@@ -0,0 +1,17 @@
+/*
+ * Sphere.cpp
+ *
+ *  Created on: 15 févr. 2011
+ *      Author: samuel
+ */
+
+#include "Sphere.h"
+
+Sphere::Sphere() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Sphere::~Sphere() {
+	// TODO Auto-generated destructor stub
+}
