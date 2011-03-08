de3bab8081da031f9943eed9f0112d3299fed4b6
Riton <riton@melix.net>
2011-03-08 21:54:48 +0100

    COmmentaires sur Noeud
    Signed-off-by: Riton <riton@melix.net>

--- src/Noeud.cpp
+++ src/Noeud.cpp
@@ -8,16 +8,31 @@ using namespace std;
  * Constructeurs & Destructeur & Affichage
  */
 
+/**
+* Constructeur pour la molécule sans position absolue
+* @param index Index numérique (position)  identifiant l'atome
+* @param type Type de l'atome (C, Ca ou N)
+*/
 Noeud::Noeud(int index, Atom type): index(index), type(type) {
 	placed = false;
 	compteur = 0;
 }
 
+/**
+* Constructeur pour la mocule avec position absolue
+* @param index Index numérique (position)  identifiant l'atome
+* @param type Type de l'atome (C, Ca ou N)
+* @param p Coordonnées de l'atome dans l'espace
+*/
 Noeud::Noeud(int index, Atom type, Point p): index(index), type(type), coord(p) {
 	placed = false;
 	compteur = 0;
 }
 
+/**
+* Affichage de l'index, du type et de l'état placé ou non
+* utilisé pour débugger le programme
+*/
 string Noeud::toString() {
 	stringstream out;
 	out << "{index: " << index << ", type: ";
@@ -32,18 +47,30 @@ string Noeud::toString() {
  * Getters
  */
 
+/**
+* Retourne l'index identifiant l'atome
+*/
 int Noeud::getIndex() {
 	return index;
 }
 
+/**
+* Retourne l'état, placé ou non, de l'atome
+*/
 bool Noeud::isPlaced() {
 	return placed;
 }
 
+/**
+* Retourne le nombre de voisins placés
+*/
 int Noeud::getCompteur() {
 	return compteur;
 }
 
+/**
+* Retourne le type de l'atome (C, Ca ou N)
+*/
 string Noeud::getStringType() {
 	if (type == Ca)
 		return "Ca";
@@ -57,10 +84,17 @@ string Noeud::getStringType() {
 	return "0";
 }
 
+/**
+* Retourne une référence de la map associant à un voisin sa distance
+*/
 map<Noeud*, double> &Noeud::getVoisins() {
 	return voisins;
 }
 
+/**
+* Retourne une représentation en chaine de caractères des voisins.
+* Utilisé pour débugger le programme
+*/
 string Noeud::getStringVoisins() {
 	stringstream out;
 
@@ -71,6 +105,9 @@ string Noeud::getStringVoisins() {
 	return out.str();
 }
 
+/**
+* Retourne les coordonnées de l'atome
+*/
 Point Noeud::getCoord(void){
 	return coord;
 }
@@ -79,39 +116,72 @@ Point Noeud::getCoord(void){
  * Setters
  */
 
-void Noeud::setCoord(Point& P) {
-	coord = P;
+/**
+* Affecte des coordonnées au noeud
+* @param p Coordonnées
+*/
+void Noeud::setCoord(Point& p) {
+	coord = p;
 }
 
+/**
+* Affecte des coordonnées au noeud et le considère comme placé
+* @param p Coordonnées
+*/
 void Noeud::affecter(Point& P) {
 	setCoord(P);
 	setPlaced();
 }
 
+/**
+* Considère l'atome comme placé
+*/
 void Noeud::setPlaced() {
 	placed = true;
 }
 
+/**
+* Considère l'atome comme non-placé
+*/
 void Noeud::unPlaced(){
 	placed=false;
 }
 
+/**
+* Ajoute un voisin avec sa distance à l'atome
+* @param dist La distance entre les deux atomes
+* @param n Pointeur vers le noeud voisin
+*/
 void Noeud::addVoisin(double dist, Noeud *n) {
 	voisins.insert(pair<Noeud*, double>(n, dist));
 }
 
+/**
+* Ajoute un voisin, la distance est calculée 
+* à partir des coordonnée des deux atomes
+* @param n Pointeur vers le noeud voisin
+*/
 void Noeud::addVoisin(Noeud *n) {
 	addVoisin(coord.dist(n->coord), n);
 }
 
+/**
+* Incrémente le compteur du nombre de voisins placés
+*/
 void Noeud::incrCompteur() {
 	compteur++;
 }
 
+/**
+* Décrément le compteur du nombre de voisins placés
+*/
 void Noeud::decrCompteur() {
 	compteur--;
 }
 
+/**
+* TODO
+*/
 Intersection Noeud::trouverIntersection(void) {
 
 	Sphere s1;
